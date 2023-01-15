540dcdc27d23015dff450219e95f13aca15e3897
Humberto <hummberton@gmail.com>
2023-01-15 01:09:17 -0300

    DELETE

--- src/main/java/io/github/machadoborges/quarkus/rest/UserResource.java
+++ src/main/java/io/github/machadoborges/quarkus/rest/UserResource.java
@@ -31,4 +31,23 @@ public class UserResource {
         PanacheQuery<User> query = User.findAll();
         return Response.ok(query.list()).build();
     }
+
+    @DELETE
+    @Path("{id}")
+    @Transactional
+    public Response deleteUser(@PathParam("id") Long id){
+        User user = User.findById(id);
+
+        if (user != null) {
+            user.delete();;
+            return Response.ok().build();
+        }
+        return Response.status(Response.Status.NOT_FOUND).build();
+    }
+
+    @PUT
+    @Path("{id}")
+    public Response updateUser(@PathParam("id") Long id, CreateUserRequest userData){
+        return Response.ok().build();
+    }
 }
